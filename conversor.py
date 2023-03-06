57272188724cfb1c7512c08ebc23358cd1515988
MajorCrayon7047 <110138925+MajorCrayon7047@users.noreply.github.com>
2023-03-05 22:31:27 -0300

    cambio de GUI consola

--- conversor.py
+++ conversor.py
@@ -2,17 +2,29 @@ from conversorClass import Conversor
 
 c = Conversor()
 while True:
-    n = input('DEC(d), BIN(b), OCT(o) y HEX(h): ').lower()
-    match n:
-        case "d":
-            resultado = c.dec(int(input('Numero decimal:    ')), int(input('Base a convertir:   ')))
-            print(resultado)
-        case 'b':
-            resultado = c.bin(input('Numero BINARIO: '), input('Base a convertir: '))
-            print(resultado)
-        case 'o':
-            resultado = c.oct(input('Numero OCTAL: '), input('Base a convertir: '))
-            print(resultado)
-        case 'h':
-            resultado = c.hex(input('Numero HEXADECIMAL: ').lower(), input('Base a convertir: '))
-            print(resultado)
+    n = input('En que base esta el numero?\nDEC(d), BIN(b), OCT(o) y HEX(h): ').lower()
+    try:
+        match n:
+            case "d":
+                num = input('Numero en decimal:    ')
+                b = input('Base a convertir:   ')
+                resultado = c.dec(int(num), int(b))
+                print(f"Resultado en base ({b}): {resultado}\n\n")
+            case 'b':
+                num = input('Numero en BINARIO: ')
+                b = input('Base a convertir: ')
+                resultado = c.bin(num, b)
+                print(f"Resultado en base ({b}): {resultado}\n\n")
+            case 'o':
+                num = input('Numero en OCTAL: ')
+                b = input('Base a convertir: ')
+                resultado = c.oct(num, b)
+                print(f"Resultado en base ({b}): {resultado}\n\n")
+            case 'h':
+                num = input('Numero en HEXADECIMAL: ').lower()
+                b = input('Base a convertir: ')
+                resultado = c.hex(num, b)
+                print(f"Resultado en base ({b}): {resultado}\n\n")
+    except Exception as err:
+        #print(err)
+        print("Ocurrio un error porfavor ingrese los datos correctamente\n---------------\n\n")
